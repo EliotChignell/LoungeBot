ce6aa505f6729a77f46bb9095bb6f6b942245450
EliotChignell <chignelleliot@gmail.com>
2019-01-30 07:32:45 +0000

    hangman working + voting now works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const MyJSONAPI = require('myjson-api');
+const request = require('request');
 
 // JSON files
 const secrets = require("./secrets.json");
@@ -41,6 +42,9 @@ client.once("ready", () => {
 client.on('message', async message => {
   
   if (!message.content.startsWith(prefix) || message.author.bot || !message.guild) return;
+
+  if (message.guild.me.hasPermission("MANAGE_NICKNAMES")) message.guild.me.setNickname('');
+  if (!client.points.get("rewarded")) client.points.set("rewarded",[]);
 
   // Uploading help.json
   MyJSONAPI.update('1d3p5k', require('./docs/help.json'));
@@ -82,7 +86,7 @@ client.on('message', async message => {
     case 'help':
       sendEmbed = true;
       eTitle = "Help";
-      eDescription = "These are the commands/games that you can use/play:```prolog\nNormal Commands:\n  help ping uptime bal\n\nGames\n  'Hangman': l;hangman start```";
+      eDescription = "These are the commands/games that you can use/play:```prolog\nNormal Commands:\nhelp ping uptime bal board vote reward\n\nGames\n'Hangman': l;hangman start```";
       break;
 
     case "uptime":
@@ -114,6 +118,92 @@ client.on('message', async message => {
         eDescription = client.points.get(message.author.id, 'points')+' credits';
       }
       break;
+
+    case 'vote':
+      sendEmbed = true;
+      eTitle = "Daily voting";
+      eDescription = "https://discordbots.org/bot/442184461405126656\nYou can get rewards for voting once every day!\nAfter voting, type `l;reward` to get your voting reward.";
+      break;
+
+    case 'reward':
+
+      var options = {                 
+        method: 'GET',             
+        uri: 'https://discordbots.org/api/bots/442184461405126656/check?userId='+message.author.id,
+        json: true,                   
+        headers: {               
+          'Authorization': secrets.dbl                  
+        }
+      };
+
+      request(options, (e,r,b) => {
+        console.log(client.points.get("rewarded"));
+        if (b.voted == 0 && client.points.get("rewarded").includes(message.author.id)) client.points.remove("rewarded", message.author.id);
+        if (b.voted == 1 && !client.points.get("rewarded").includes(message.author.id)) {
+          let rewardedCredits = 250+Math.floor(client.points.get(message.author.id, "points")/10);
+          client.points.math(message.author.id, "+", rewardedCredits, "points");
+          client.points.push("rewarded", message.author.id);
+          embed = new Discord.RichEmbed()
+            .setTitle('Thanks for voting!')
+            .setAuthor("LoungeBot",client.user.displayAvatarURL)
+            .setColor(0xff8300)
+            .setDescription("Make sure to vote every day to get rewards!\nYou recieved "+rewardedCredits+" credits!")
+            .setFooter("l;[command]")
+            .setTimestamp();
+            message.author.send(embed);
+        } else if (b.voted == 0) {
+          message.channel.send("You have not voted!");
+        }
+      });
+      break;
+    
+    case 'leaderboard':
+    case 'board':
+
+      if (args[0] || !message.guild) {
+        let sorted = client.points.array().sort((a, b) => a.points - b.points).reverse().splice(0,10);
+        sendEmbed = false;
+        let rDescription = "The Worldwide Top 10:```diff\n";
+        /*for (const data of sorted) {
+          rDescription += "\n- "+client.users.get(parseInt(data.id)).tag+"\n+ "+data.points+" credits";
+        }*/
+        for (var i=0;i<sorted.length;i++) {
+          if (client.users.get(sorted[i].id)) rDescription += "\n- "+client.users.get(sorted[i].id).tag+"\n+ "+sorted[i].points+" credits";
+        }
+        rDescription += '```';
+        embed = new Discord.RichEmbed()
+          .setTitle("Credits Leaderboard")
+          .setAuthor("LoungeBot",client.user.displayAvatarURL)
+          .setColor(0xff8300)
+          .setDescription(rDescription)
+          .setFooter("l;[command]")
+          .setTimestamp();
+        
+        message.channel.send(embed);
+      } else if (!args[0] && message.guild) {
+        let foundUsers = [];
+        let guildIDs = [];
+        message.guild.members.forEach(member => {
+          if (client.points.has(member.id)) guildIDs.push(member.id);
+        });
+        let sorted = client.points.array().filter(p => guildIDs.includes(p.id)).sort((a, b) => a.points - b.points).reverse().splice(0,10);
+        let rDescription = "The Top 10 of the server `"+message.guild.name+"`:```diff\n";
+        for (const data of sorted) {
+          rDescription += "\n- "+client.users.get(data.id).tag+"\n+ "+data.points+" credits";
+        }
+        rDescription += '```\nType `ch board world` to find out the worldwide standings.';
+        sendEmbed = false;
+        embed = new Discord.RichEmbed()
+          .setTitle("Credits Leaderboard")
+          .setAuthor("LoungeBot",client.user.displayAvatarURL)
+          .setColor(0xff8300)
+          .setDescription(rDescription)
+          .setFooter("l;[command]")
+          .setTimestamp();
+        message.channel.send(embed);
+      }
+      break;
+  
 
     // Games
     case 'hangman':
@@ -174,7 +264,7 @@ client.on('message', async message => {
         } else if (games.hangman.get(message.author.id, "word").includes(args[0].toLowerCase())) { // Correct
           
           games.hangman.push(message.author.id, args[0].toLowerCase(), "lettersCorrect");
-
+          console.log(games.hangman.get(message.author.id, "nonDuplicates").length+" "+games.hangman.get(message.author.id, "lettersCorrect").length);
           if (games.hangman.get(message.author.id, "nonDuplicates").length == games.hangman.get(message.author.id, "lettersCorrect").length) { // Won
 
             games.hangman.set(message.author.id, false, "inGame");
@@ -182,7 +272,7 @@ client.on('message', async message => {
             sendEmbed = true;
             eTitle = "Hangman";
             eDescription = "You Won!\nThe word was: "+games.hangman.get(message.author.id, "word").join('')+"\nTherefore, you recieve "+50*games.hangman.get(message.author.id, "word").length+" credits!";
-            client.points.math(message.author.id, "+", 50*localInformation[message.author.id].word.length, "points");
+            client.points.math(message.author.id, "+", 50*games.hangman.get(message.author.id, "word").length, "points");
 
             break;
           }
